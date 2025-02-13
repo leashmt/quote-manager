f4683e7b64a0468c5c4c4f975aac2d7a2b20fa36
Léa Schmitt <146575913+leashmt@users.noreply.github.com>
2025-02-13 09:46:41 +0100

    Initial commit

---
+++ src/main/java/com/example/quoteManager/QuoteManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.quoteManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quoteManager/QuoteManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quoteManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
